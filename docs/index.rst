cc187218c5d81a28cdbe1f1225b5d2e09f209f58
Simon Sapin <simon.sapin@exyr.org>
2012-07-16 20:18:11 +0200

    Add some docs

--- docs/index.rst
+++ docs/index.rst
@@ -1,3 +1,5 @@
+.. module:: flask_weasyprint
+
 .. include:: ../README.rst
 
 
@@ -10,10 +12,49 @@ and working, just install the extension with pip::
     $ pip install Flask-WeasyPrint
 
 
-Documentation
--------------
+Quickstart
+----------
 
-**TODO**
+Just import :func:`~flask_weasyprint.HTML` and :func:`~flask_weasyprint.CSS`
+from ``flask_weasyprint`` rather than ``weasyprint``, and use them from
+within a Flask :ref:`request context <flask:request-context>`.
+:func:`render_pdf` is a helper function to make a :class:`~flask.Response`
+with the correct MIME type.
+
+.. code-block:: python
+
+    from flask import Flask, render_template, url_for
+    from flask_weasyprint import HTML, render_pdf
+
+    app = Flask(__name__)
+
+    @app.route('/lorem/ipsum_<int:lipsum_id>/'):
+    def lipsum(lipsum_id):
+        # A normal HTML view
+        data = ...
+        return render_template('lipsum.html', **data)
+
+    @app.route('/lorem/ipsum_<int:lipsum_id>.pdf'):
+    def lipsum_pdf(lipsum_id):
+        # Make a PDF from another view
+        return render_pdf(url_for('lipsum', lipsum_id=lipsum_id))
+
+    # Alternatively, if the PDF does not have a matching HTML page:
+
+    @app.route('/lorem/ipsum_<int:lipsum_id>.pdf'):
+    def lipsum_pdf(lipsum_id):
+        # Make a PDF straight from HTML in a string.
+        data = ...
+        return render_pdf(HTML(string=render_template('lipsum.html', **data)))
+
+
+API
+---
+
+.. autofunction:: make_url_fetcher
+.. autofunction:: HTML(guess=None, **kwargs)
+.. autofunction:: CSS(guess=None, **kwargs)
+.. autofunction:: render_pdf
 
 
 Changelog
--- flask_weasyprint/__init__.py
+++ flask_weasyprint/__init__.py
@@ -22,6 +22,20 @@ __all__ = ['VERSION', 'make_url_fetcher', 'HTML', 'CSS', 'render_pdf']
 
 
 def make_url_fetcher():
+    """Return an function suitable as a ``url_fetcher`` in WeasyPrint.
+    You generally don’t need to call this directly.
+
+    This requires a Flask :ref:`request context <flask:request-context>`.
+    The current application and its root URL (based on ``wsgi.url_scheme``,
+    ``HTTP_HOST`` and ``SCRIPT_NAME``) are taken from this context when the
+    fetcher is first created. The context is not needed when the fetcher
+    is used (when rendering with WeasyPrint).
+
+    Requests for URLs below the application’s root URL are made directly
+    with Python calls as the WSGI level, without going through the network.
+    Other URLs are fetched normally.
+
+    """
     url_root = request.url_root
     client = current_app.test_client()
     def flask_url_fetcher(url):
@@ -49,26 +63,56 @@ def make_url_fetcher():
     return flask_url_fetcher
 
 
-def _wrap(class_):
-    def wrapper(*args, **kwargs):
-        if args:
-            guess = args[0]
-            args = args[1:]
-        else:
-            guess = kwargs.pop('guess', None)
-        if guess is not None and not hasattr(guess, 'read'):
-            # Assume a (possibly relative) URL
-            guess = urlparse.urljoin(request.url, guess)
-        kwargs['url_fetcher'] = make_url_fetcher()
-        return class_(guess, *args, **kwargs)
-    wrapper.__name__ = class_.__name__
-    return wrapper
+def _wrapper(class_, *args, **kwargs):
+    if args:
+        guess = args[0]
+        args = args[1:]
+    else:
+        guess = kwargs.pop('guess', None)
+    if guess is not None and not hasattr(guess, 'read'):
+        # Assume a (possibly relative) URL
+        guess = urlparse.urljoin(request.url, guess)
+    kwargs['url_fetcher'] = make_url_fetcher()
+    return class_(guess, *args, **kwargs)
 
-HTML = _wrap(weasyprint.HTML)
-CSS = _wrap(weasyprint.CSS)
+
+def HTML(*args, **kwargs):
+    """Like `weasyprint.HTML()
+    <http://weasyprint.org/using/#the-weasyprint-html-class>`_ but:
+
+    * :func:`make_url_fetcher` is used to create an ``url_fetcher``
+    * If ``guess`` is not a file object, it is an URL relative to the current
+      request context.
+      This means that you can just pass a result from :func:`flask.url_for`.
+
+    This requires a Flask request context.
+
+    """
+    return _wrapper(weasyprint.HTML, *args, **kwargs)
+
+
+def CSS(*args, **kwargs):
+    return _wrapper(weasyprint.CSS, *args, **kwargs)
+CSS.__doc__ = HTML.__doc__.replace('HTML', 'CSS').replace('html', 'css')
 
 
 def render_pdf(html, stylesheets=None, download_filename=None):
+    """Render a PDF to a response with the correct ``Content-Type`` header.
+
+    :param html:
+        Either a :class:`weasyprint.HTML` object or an URL to be passed
+        to :func:`flask_weasyprint.HTML`. The latter case requires
+        a request context.
+    :param stylesheets:
+        A list of user stylesheets, passed to
+        :meth:`~weasyprint.HTML.write_pdf`
+    :param download_filename:
+        If provided, the ``Content-Disposition`` header is set so that most
+        web browser will show the "Save as…" dialog with the value a default
+        filename.
+    :returns: a :class:`flask.Response` object.
+
+    """
     if not hasattr(html, 'write_pdf'):
         html = HTML(html)
     pdf = html.write_pdf(stylesheets=stylesheets)
